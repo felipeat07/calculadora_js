fb98e861e4473e542e5e35a626ebbc17799845a5
Felipe Tavares <felipeat07@gmail.com>
2022-02-12 12:35:34 +0000

    iniciando a classe em js

---
+++ script.js
@@ -0,0 +1,27 @@
+class Calculator{
+
+    constructor(){
+        this.upperValue = document.querySelector('#upper-number');
+        this.resultValue = document.querySelector('#result-number');
+        this.reset=0;
+    }
+
+    btnPress(){
+
+    }
+
+}
+
+//iniciando oobjeto
+
+let calc = new Calculator;
+
+//iniciando todos os botoes
+
+let buttons = document.querySelectorAll('.btn');
+
+//Adicionando evento de click em todos os botoes e o metodo btnPress
+
+for(i=0, buttons.length>i , i++){
+    buttons[i].addEventListener('CLick', calc.btnPress);
+}
