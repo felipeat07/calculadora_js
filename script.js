a4f959c63e1919bf68cbf0faf192d43e723fa05a
Felipe Tavares <felipeat07@gmail.com>
2022-02-19 22:09:54 +0000

    finalizando os operadores e correção de bugs

--- script.js
+++ script.js
@@ -24,6 +24,29 @@ class Calculator{
         }
       }
 
+      sum(n1,n2){
+        return parseFloat(n1) + parseFloat(n2);      
+      }
+
+      subtratiction(n1,n2){
+        return parseFloat(n1) - parseFloat(n2);      
+      }
+
+      multiplication(n1,n2){
+        return parseFloat(n1) * parseFloat(n2);      
+      }
+
+      division(n1,n2){
+        return parseFloat(n1) / parseFloat(n2);      
+      }
+
+
+      //atualiza valores
+      refreshValues(total){
+        this.upperValue.textContent = total;
+        this.resultValue.textContent = total;
+      }
+
     //resolve a operação
     resolution(){
 
@@ -35,16 +58,50 @@ class Calculator{
 
         for(let i=0; i <= upperValueArray.length; i++ ){
           let actualItem = upperValueArray[i];
+          let operation = 0;
 
-          if(actualItem == '+'){
-            result = parseFloat(upperValueArray[i-1]) + parseFloat(upperValueArray[i-1]);
+          if(actualItem == 'x'){
+            result = calc.multiplication(upperValueArray[i-1],upperValueArray[i+1]);
+            operation = 1;
+          }
+
+          else if(actualItem == '/'){
+            result = calc.division(upperValueArray[i-1],upperValueArray[i+1]);
+            operation = 1;
+          }
+
+          else if(!upperValueArray.includes('x') && !upperValueArray.includes('/')){
+
+            if(actualItem == '+'){
+              result = calc.sum(upperValueArray[i-1],upperValueArray[i+1]);
+              operation = 1;
+            }
+  
+            else if(actualItem == '-'){
+              result = calc.subtratiction(upperValueArray[i-1],upperValueArray[i+1]);
+              operation = 1;
+            }
+
+          }
+
+          //atualiza array para proxima operação
+          if(operation){
+            //indice anterior no resultado da operação
+            upperValueArray[i-1]=result;
+            //remove os itens já utilizados na operação
+            upperValueArray.splice(i,2);
+            //atualiza o valor do indice
+            i=0;
           }
 
         }
 
-        this.upperValue.textContent = result;
-        this.resultValue.textContent = result;
+        if(result){
+          calc.reset = 1;
+        }
 
+        //atualizar os totais
+        calc.refreshValues(result);
 
       }
 
@@ -56,10 +113,19 @@ class Calculator{
         //verificando se tem só numeros
         var reg = new RegExp('^\\d+$');
 
+        //reseta o display
+        if(calc.reset && reg.test(input)){
+          upperValue = '0';
+        }
+        
+        //limpa a prop reset
+        calc.reset = 0;
+
         if(input == 'AC'){
           calc.clearValues();
         } 
         
+
         else if(input == '='){
           calc.resolution();
         }
@@ -77,18 +143,16 @@ class Calculator{
 
         
             if(upperValue == '0'){
+              if(reg.test(input)){
                 calc.upperValue.textContent = input;
+              }
             } else{
                 calc.upperValue.textContent += input;
             }
 
           
         }
-
-      
-
     }
-
 }
 
 //iniciando oobjeto
